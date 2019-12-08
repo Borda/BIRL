b42e17202418474b067414834d7e6f54aa338931
Jiri Borovec <jirka.borovec@seznam.cz>
2019-12-08 23:23:02 +0100

    drop enlighten

--- bm_CIMA/run-SOTA-experiments.sh
+++ bm_CIMA/run-SOTA-experiments.sh
@@ -14,46 +14,46 @@ python ./bm_experiments/bm_bUnwarpJ.py \
      --run_comp_benchmark \
      -Fiji ~/TEMP/Applications/Fiji.app/ImageJ-linux64 \
      -cfg ./configs/ImageJ_bUnwarpJ_histol.yaml \
-     --visual --unique --nb_workers $jobs
+     --nb_workers $jobs --unique
 
 python ./bm_experiments/bm_bUnwarpJ.py \
      -t $table -d $dataset -o $results \
      --run_comp_benchmark \
      -Fiji ~/TEMP/Applications/Fiji.app/ImageJ-linux64 \
      -cfg ./configs/ImageJ_bUnwarpJ-SIFT_histol.yaml \
-     --visual --unique --nb_workers $jobs
+     --nb_workers $jobs --unique
 
 python ./bm_experiments/bm_DROP2.py \
      -t $table -d $dataset -o $results \
      --run_comp_benchmark \
      -DROP ~/TEMP/Applications/DROP2/dropreg \
      -cfg ./configs/DROP2.txt \
-     --visual --unique --nb_workers $jobs
+     --nb_workers $jobs --unique
 
 python ./bm_experiments/bm_elastix.py \
      -t $table -d $dataset -o $results \
      --run_comp_benchmark \
      -elastix ~/Applications/elastix/bin \
      -cfg ./configs/elastix_bspline.txt \
-     --visual --unique --nb_workers $jobs
+     --nb_workers $jobs --unique
 
 python ./bm_experiments/bm_rNiftyReg.py \
      -t $table -d $dataset -o $results \
      --run_comp_benchmark \
      -R ~/TEMP/Applications/R-3.5.3/bin/Rscript \
      -script ./scripts/Rscript/RNiftyReg_linear.r \
-     --visual --unique --nb_workers $jobs
+     --nb_workers $jobs --unique
 
 python ./bm_experiments/bm_RVSS.py \
      -t $table -d $dataset -o $results \
      --run_comp_benchmark \
      -Fiji ~/TEMP/Applications/Fiji.app/ImageJ-linux64 \
      -cfg ./configs/ImageJ_RVSS_histol.yaml \
-     --visual --unique --nb_workers $jobs
+     --nb_workers $jobs --unique
 
 python ./bm_experiments/bm_ANTs.py \
      -t $table -d $dataset -o $results \
      --run_comp_benchmark \
      -ANTs ~/TEMP/Applications/antsbin/bin \
      -cfg ./configs/ANTs_SyN.txt \
-     --visual --unique --nb_workers $jobs
+     --nb_workers $jobs --unique
