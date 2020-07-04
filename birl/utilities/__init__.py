254d8847fd9e8a57a3b445c03f4c5988abd7b06a
Jirka <jirka.borovec@seznam.cz>
2020-07-04 12:28:41 +0200

    fix init

--- birl/utilities/__init__.py
+++ birl/utilities/__init__.py
@@ -1,27 +1,39 @@
 import os
 import subprocess
 
-import matplotlib
-import numpy as np
-import pandas as pd
+try:
+    import matplotlib
+except (ModuleNotFoundError, ImportError):
+    print('Package `matplotlib` which shall be configured are missing...')
+else:
+    CMD_TRY_MATPLOTLIB = 'python -c "from matplotlib import pyplot; pyplot.close(pyplot.figure())"'
+    # in case you are running on machine without display, e.g. server
+    if not os.environ.get('DISPLAY', '') and matplotlib.rcParams['backend'] != 'agg':
+        print('No display found. Using non-interactive Agg backend')
+        matplotlib.use('Agg')
+    # _tkinter.TclError: couldn't connect to display "localhost:10.0"
+    elif subprocess.call(CMD_TRY_MATPLOTLIB, stdout=None, stderr=None, shell=True):
+        print('Problem with display. Using non-interactive Agg backend')
+        matplotlib.use('Agg')
 
 
-CMD_TRY_MATPLOTLIB = 'python -c "from matplotlib import pyplot; pyplot.close(pyplot.figure())"'
-# in case you are running on machine without display, e.g. server
-if not os.environ.get('DISPLAY', '') and matplotlib.rcParams['backend'] != 'agg':
-    print('No display found. Using non-interactive Agg backend')
-    matplotlib.use('Agg')
-# _tkinter.TclError: couldn't connect to display "localhost:10.0"
-elif subprocess.call(CMD_TRY_MATPLOTLIB, stdout=None, stderr=None, shell=True):
-    print('Problem with display. Using non-interactive Agg backend')
-    matplotlib.use('Agg')
-
+try:
+    import numpy as np
+except (ModuleNotFoundError, ImportError):
+    print('Package `numpy` which shall be configured are missing...')
+else:
 # parse the numpy versions
-np_version = [int(i) for i in np.version.full_version.split('.')]
-# comparing strings does not work for version lower 1.10
-if np_version >= [1, 14]:
-    # np.set_printoptions(sign='legacy')
-    np.set_printoptions(legacy='1.13')
+    np_version = [int(i) for i in np.version.full_version.split('.')]
+    # comparing strings does not work for version lower 1.10
+    if np_version >= [1, 14]:
+        # np.set_printoptions(sign='legacy')
+        np.set_printoptions(legacy='1.13')
 
-# default display size was changed in pandas v0.23
-pd.set_option('display.max_columns', 20)
+
+try:
+    import pandas as pd
+except (ModuleNotFoundError, ImportError):
+    print('Package `pandas` which shall be configured are missing...')
+else:
+    # default display size was changed in pandas v0.23
+    pd.set_option('display.max_columns', 20)
